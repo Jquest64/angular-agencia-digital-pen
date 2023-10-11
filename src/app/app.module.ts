003c701ba0ae7d45db2e374aa0cf026181d7efff
Marcio Farias Pinto <mjquest64@outlook.com>
2023-10-11 13:51:52 -0300

    Início do site da agência digital pen
    Projeto do site agência digital pen.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { SectionComponent } from './components/section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
